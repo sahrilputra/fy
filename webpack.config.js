52e9cca333188ada25954e88f952b55cb207506a
sahrilputra <iam.sahrilputra@gmail.com>
2022-09-30 20:16:34 +0700

    change

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path =
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'output'),
         filename: 'bundle.js',
